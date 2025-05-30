d5cd6787bb64957935c90adcb68127c1f69e5428
RMaxiG <maxiguzman06@gmail.com>
2025-05-30 16:54:03 -0300

    feat: Creacion componentes NavBar y Home imlementacion en app.jsx [RodriMG]

---
+++ Tp5-Grupo4/src/Views/Home.jsx
@@ -0,0 +1,9 @@
+const Inicio = () => {
+    return(
+        <div>
+            <h1>Sistema Integral de Gestión de Alumnos</h1>
+            <h2>Administración de Datos Estudiantiles</h2>
+        </div>
+    )
+}
+export default Inicio
---
+++ Tp5-Grupo4/src/components/NavBar.jsx
@@ -0,0 +1,35 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Styles from "../Style/NavBar.module.css"
+
+const NavBar = () => {
+  return (
+    <nav className={Styles.navBarContainer}>
+      <div className={Styles.horizontal}>
+      <h1 className={Styles.navbarTitle}>Gestión de Alumnos</h1>
+      <ul  className={Styles.navbarList}>
+        <li>
+          <Link to="/"  className={Styles.navbarLink}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/ListAlumnos"  className={Styles.navbarLink}>
+            Lista de Alumnos
+          </Link>
+        </li>
+        <li>
+          <Link to="/NewAlumno"  className={Styles.navbarLink}>
+            Nuevo Alumno
+          </Link>
+        </li>
+        <li>
+          <Link to="/acercad"  className={Styles.navbarLink}>
+            Acerda de Nosotros
+          </Link>
+        </li>
+      </ul>
+      </div>
+    </nav>
+  );
+};
+export default NavBar;
