1ef512a153f37c367c7341db8e2b5e7ec20e728b
MarcosM09 <marcos.antonio.mendez2002@gmail.com>
2025-05-30 17:18:11 -0300

    feat: estructura inicial de app con rutas y gestión de alumnos [MarcosM09]

--- Tp5-Grupo4/src/components/NavBar.jsx
+++ Tp5-Grupo4/src/components/NavBar.jsx
@@ -5,29 +5,29 @@ const NavBar = () => {
   return (
     <nav className={Styles.navBarContainer}>
       <div className={Styles.horizontal}>
-      <h1 className={Styles.navbarTitle}>Gestión de Alumnos</h1>
-      <ul  className={Styles.navbarList}>
-        <li>
-          <Link to="/"  className={Styles.navbarLink}>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/ListAlumnos"  className={Styles.navbarLink}>
-            Lista de Alumnos
-          </Link>
-        </li>
-        <li>
-          <Link to="/NewAlumno"  className={Styles.navbarLink}>
-            Nuevo Alumno
-          </Link>
-        </li>
-        <li>
-          <Link to="/acercad"  className={Styles.navbarLink}>
-            Acerda de Nosotros
-          </Link>
-        </li>
-      </ul>
+        <h1 className={Styles.navbarTitle}>Gestión de Alumnos</h1>
+        <ul className={Styles.navbarList}>
+          <li>
+            <Link to="/" className={Styles.navbarLink}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/ListAlumnos" className={Styles.navbarLink}>
+              Lista de Alumnos
+            </Link>
+          </li>
+          <li>
+            <Link to="/NewAlumno" className={Styles.navbarLink}>
+              Nuevo Alumno
+            </Link>
+          </li>
+          <li>
+            <Link to="/acercad" className={Styles.navbarLink}>
+              Acerda de Nosotros
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
