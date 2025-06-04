ce3752d49b86519f25da29c980fcaf609f5a7d55
lzmar <mlrodriguez0302@gmail.com>
2025-06-04 20:35:16 -0300

    Aboutus

--- Tp5-Grupo4/src/Views/AboutUs.jsx
+++ Tp5-Grupo4/src/Views/AboutUs.jsx
@@ -3,8 +3,10 @@ const AboutUs = ()=>{
         <div>
             <h3>EL PROCESO</h3>
             <p>Crear una página web es una experiencia emocionante y desafiante. 
-            El sentimiento general es una mezcla de entusiasmo, aprendizaje y, a veces, un poco de frustración. 
-            <br></br>Es una oportunidad de llevar tus ideas a la web, conectar con otras personas y, si es para un negocio, obtener un nuevo cliente.</p>
+            El sentimiento general es una mezcla de entusiasmo, aprendizaje y, a veces, un poco de frustración.<br></br>Es una oportunidad de llevar tus ideas a la web, conectar con otras personas y, si es para un negocio, obtener un nuevo cliente. </p>
+            <br></br><br></br><h3>HISTORIA DE LA FACULTAD DE INGENIERIA</h3>
+            <p>La Universidad nace el 25 de mayo de 1972 como universidad provincial y al año siguiente se transforma en Universidad Nacional. Comienza a funcionar sobre la base de cuatro carreras: Ingeniería Agronómica, Ingeniería de Minas, Ingeniería Metalúrgica e Ingeniería Química. Su propósito fue apuntalar las actividades de explotación e industrialización de los recursos naturales de Jujuy. Las tres últimas carreras mencionadas pasan a constituir la actual Facultad de Ingeniería que, con el transcurso de los años, sumó a ellas un número importante de otras carreras.
+            Las políticas de la Universidad Nacional de Jujuy, y especialmente de la Facultad de Ingeniería, tienden a ampliar el área de influencia y a fortalecer su acción institucional, en respuesta a la demanda social creciente de formación de recursos humanos que contribuyan al desarrollo científico, tecnológico y cultural. </p>
         </div>
     )
 }
