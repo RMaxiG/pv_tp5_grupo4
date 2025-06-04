2bb7d0beb8330d994da975c2e779184e954b3662
Mili Martinez <milimartinez.uno@gmail.com>
2025-06-04 19:45:10 -0300

    modulo de estilos para UserDetails

--- Tp5-Grupo4/src/Views/UserDetails.jsx
+++ Tp5-Grupo4/src/Views/UserDetails.jsx
@@ -1,20 +1,20 @@
 import { useNavigate, useParams } from 'react-router-dom';
-
+import styles from '../Style/UserDetails.module.css';
 const DetalleAlumno = ({ lista }) => {
     const { lu } = useParams();
     const alumno = lista.find(est => est.Lu === lu);
     const volver = useNavigate();
     return (
-        <div>
-            <h2>Detalle del Alumno</h2>
-            <p><strong>LU:</strong> {alumno.Lu}</p>
-            <p><strong>Nombre:</strong> {alumno.nombre}</p>
-            <p><strong>Apellido:</strong> {alumno.apellido}</p>
-            <p><strong>Curso:</strong> {alumno.curso}</p>
-            <p><strong>Correo:</strong> {alumno.correo}</p>
-            <p><strong>Domicilio:</strong> {alumno.domicilio}</p>
-            <p><strong>Teléfono:</strong> {alumno.teléfono}</p>
-            <button onClick={() => volver("/")}>Volver</button>
+        <div className={styles.detailsCard}>
+            <h2>Datos del Alumno</h2>
+            <p className={styles.detailItem}><strong>LU:</strong> {alumno.Lu}</p>
+            <p className={styles.detailItem}><strong>Nombre:</strong> {alumno.nombre}</p>
+            <p className={styles.detailItem}><strong>Apellido:</strong> {alumno.apellido}</p>
+            <p className={styles.detailItem}><strong>Curso:</strong> {alumno.curso}</p>
+            <p className={styles.detailItem}><strong>Correo:</strong> {alumno.correo}</p>
+            <p className={styles.detailItem}><strong>Domicilio:</strong> {alumno.domicilio}</p>
+            <p className={styles.detailItem}><strong>Teléfono:</strong> {alumno.teléfono}</p>
+            <button  className={styles.backButton} onClick={() => volver("/")}>Volver</button>
         </div>
     );
 };
